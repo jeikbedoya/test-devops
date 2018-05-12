85b3fcce2e9496cfd6e04e4227646211abda7658
Jheyson Bedoya <jeik@MacBook-Air-de-Jheyson.local>
2018-05-12 14:32:31 -0500

    add change html2

--- deployments/qa.sh
+++ deployments/qa.sh
@@ -1,5 +1,5 @@
 npm install -g now
 echo "deploying..."
-URL=$(now --docker -t --public $NOW_TOKEN) 
+URL=$(now --docker --public -t $NOW_TOKEN) 
 echo "runnning acceptance on $URL"
 curl --silent -L $URL 
